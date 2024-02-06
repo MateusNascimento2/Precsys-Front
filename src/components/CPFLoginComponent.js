e9321d712956ecfadd650e02783dc3a92b00aa3b
MateusNascimento2 <mateusnascar@hotmail.com>
2024-02-06 20:58:04 -0300

    first commit

--- src/components/CPFLoginComponent.js
+++ src/components/CPFLoginComponent.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import PasswordLoginComponent from "./PasswordLoginComponent";
 
 function CPFLoginComponent() {
+  const [showPasswordLogin, setShowPasswordLogin] = useState(false);
+
+  const handleContinue = () => {
+    setShowPasswordLogin(true);
+  };
+
   return (
-    <div className="bg-white flex flex-col">
-      <h2 className="font-bold text-3xl text-center mb-8 w-[300px]">Log in para o Precsys</h2>
-      <div>
-        <form className="flex flex-col gap-4">
-          <input className="border rounded py-3 px-4" type="text" placeholder="CPF/CNPJ" required/>
-          <button type="submit" className="border rounded-md text-white text-center bg-black py-3 px-4 flex items-center justify-center gap-2 hover:opacity-85">
-            <span className="font-medium">Continuar com a Senha</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-            </svg>
-          </button>
-        </form>
-      </div>
+    <div className="flex flex-col">
+
+      {showPasswordLogin ? (
+        <PasswordLoginComponent />
+      ) : (
+        <>
+          <h2 className="font-bold text-3xl text-center mb-8 w-[300px]">Log in para o Precsys</h2>
+          <div>
+            <form className="flex flex-col gap-4">
+              <input className="border rounded py-3 px-4" type="text" placeholder="CPF/CNPJ" required />
+              <button
+                onClick={handleContinue}
+                className="border rounded-md text-white text-center bg-black py-3 px-4 flex items-center justify-center gap-2 hover:opacity-85"
+              >
+                <span className="font-medium">Continuar com a Senha</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                </svg>
+              </button>
+            </form>
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
 export default CPFLoginComponent;
